b7c9788ea84b1f316903db8981d4613339897440
John-David Wuarin <john.dwuarin@gmail.com>
2013-04-20 19:09:38 +0200

    Corrected typo and a couple other small things

--- src/main/java/com/tradable/exampleApps/useMarketDepth/UseMarketDepthModule.java
+++ src/main/java/com/tradable/exampleApps/useMarketDepth/UseMarketDepthModule.java
@@ -1,11 +1,15 @@
 package com.tradable.exampleApps.useMarketDepth;
 
 
+import java.awt.Color;
+
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
 
 import com.tradable.api.component.WorkspaceModule;
 import com.tradable.api.component.WorkspaceModuleProperties;
@@ -25,8 +29,11 @@ public class UseMarketDepthModule extends JPanel implements WorkspaceModule, Mar
 	private static final long serialVersionUID = 1L; 
 	private MarketDepthService marketDepthService;
 	private MarketDepthSubscription marketDepthSubscription;
-	private JScrollPane scrollPane;
-	private JTextPane textPane;
+
+	private JLabel lblBidVWAP;
+	private JLabel lblBidVWAPQuant;
+	private JLabel lblAskVWAP;
+	private JLabel lblAskVWAPQuant;
 	
 	public UseMarketDepthModule(MarketDepthService marketDepthService) {
 		setLayout(null);
@@ -37,19 +44,31 @@ public class UseMarketDepthModule extends JPanel implements WorkspaceModule, Mar
 		//creates an empty Jlabel that will contain the accountID upon 
 		//catching an event.
 		
-		scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(0, 0, 400, 400);
-		add(scrollPane);
+		lblBidVWAP = new JLabel("BW");
+		lblBidVWAP.setHorizontalAlignment(SwingConstants.LEADING);
+		lblBidVWAP.setForeground(Color.BLACK);
+		lblBidVWAP.setBounds(64, 28, 141, 31);
+		add(lblBidVWAP);
 		
-		textPane = new JTextPane();
-		textPane.setEditable(false);
-		scrollPane.setViewportView(textPane);
+		
 		
 		this.marketDepthService = marketDepthService;	
 		this.marketDepthSubscription = this.marketDepthService.createMarketDepthSubscription();
 		this.marketDepthSubscription.addSymbol("EURUSD");
 		this.marketDepthSubscription.setListener(this);
+		
+		lblBidVWAPQuant = new JLabel("BidQuant");
+		lblBidVWAPQuant.setHorizontalAlignment(SwingConstants.LEADING);
+		lblBidVWAPQuant.setBounds(64, 80, 319, 31);
+		add(lblBidVWAPQuant);
+		
+		lblAskVWAP = new JLabel("AskVWAP");
+		lblAskVWAP.setBounds(64, 201, 141, 31);
+		add(lblAskVWAP);
+		
+		lblAskVWAPQuant = new JLabel("AskQuant");
+		lblAskVWAPQuant.setBounds(64, 259, 319, 31);
+		add(lblAskVWAPQuant);
 			
 	}
 
@@ -86,32 +105,64 @@ public class UseMarketDepthModule extends JPanel implements WorkspaceModule, Mar
 	@Override
 	public void marketDepthUpdated(MarketDepthEvent event) {
 		
+		
+		int vtt = 25000000; //volumeToTrade
+		int bidTradeQuantity = 0;
+		double bidVWP = 0; //non averaged yet
+		double bidVWAP = 0;
+	
         String symbol = event.getSymbol();
-        for (Quote bid : event.getSource().getBids(symbol)) {
-        	
-    		try {
-				textPane.getDocument().insertString(textPane.getDocument().getLength() ,
-						"order of size: " + String.valueOf(bid.getSize()) + 
-								" moved the bid price to: " + String.valueOf(bid.getPrice())
-								+ "\n", null);
-			} catch (BadLocationException e) {
-				e.printStackTrace();
+        
+		for (Quote bid : event.getSource().getBids(symbol)){
+			
+			if(bid.getPrice() > vtt){
+				bidTradeQuantity += vtt;
+				bidVWP += vtt*bid.getPrice();
+				vtt = 0;
 			}
-    		
-        }
-
-        for (Quote ask : event.getSource().getOffers(symbol)) {
-
-    		try {
-				textPane.getDocument().insertString(textPane.getDocument().getLength() ,
-						"order of size: " + String.valueOf(ask.getSize()) + 
-								" moved the ask price to: " + String.valueOf(ask.getPrice())
-								+ "\n", null);
-			} catch (BadLocationException e) {
-				e.printStackTrace();
+			else{
+				bidTradeQuantity += bid.getSize();
+				bidVWP += bid.getPrice()*bid.getSize();
+				vtt -= bid.getSize();
 			}
-        	
-        }
+			
+			if(vtt == 0)
+				break;
+		}
+        
+		bidVWAP = bidVWP/bidTradeQuantity;
+		lblBidVWAP.setText("Bid VWAP: " + String.valueOf(bidVWAP));
+		lblBidVWAPQuant.setText("found bid liquidity for " + String.valueOf(bidTradeQuantity)
+				+ " of " + String.valueOf(vtt + bidTradeQuantity));
+		
+		
+		vtt += bidTradeQuantity; //volumeToTrade
+		int askTradeQuantity = 0;
+		double askVWP = 0; //non averaged yet
+		double askWAP = 0;
+	
+        
+		for (Quote ask : event.getSource().getOffers(symbol)){
+			
+			if(ask.getPrice() > vtt){
+				askTradeQuantity += vtt;
+				askVWP += vtt*ask.getPrice();
+				vtt = 0;
+			}
+			else{
+				askTradeQuantity += ask.getSize();
+				askVWP += ask.getPrice()*ask.getSize();
+				vtt -= ask.getSize();
+			}
+			
+			if(vtt == 0)
+				break;
+		}
+        
+		askWAP = askVWP/askTradeQuantity;
+		lblAskVWAP.setText("Ask VWAP: " + String.valueOf(askWAP));
+		lblAskVWAPQuant.setText("found ask liquidity for " + String.valueOf(askTradeQuantity)
+				+ " of " + String.valueOf(vtt + askTradeQuantity));
 
 		
 	}
